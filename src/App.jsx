e9852cffe51fded7f34e33b2f6ce888f50f13689
Ujjwal <manitripathiujjwal@gmail.com>
2021-10-01 19:36:34 +0530

    Added JSON Fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+// import { robots } from './robots';
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     };
+    // console.log('constructor');
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+
+    // console.log('component Did Mount');
   }
 
   onSearchChange = (event) => {
@@ -21,9 +33,13 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    }
+    // console.log('Render');
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f2">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
